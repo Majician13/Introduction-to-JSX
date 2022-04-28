e83e0803cc292b5e5f1340d87eb0cb87f310d117
Majician13 <44847744+Majician13@users.noreply.github.com>
2022-04-28 15:44:19 -0400

    Learning JSX/React Inserting h1 header and unordered list in JS.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Three Magicians</h1>
+    <ul>
+      <li>Harry Houdini</li>
+      <li>David Copperfield</li>
+      <li>Dan Harlan</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
